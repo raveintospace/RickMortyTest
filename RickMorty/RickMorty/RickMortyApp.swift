99672a8716e0ec35742967809f2f859daa32602e
Oriol Angelet <uriaeri@gmail.com>
2025-11-13 08:53:24 +0100

    Initial Commit: Folders structure & RMColors

---
+++ RickMorty/RickMorty/RickMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickMortyApp.swift
+//  RickMorty
+//
+//  Created by Uri on 13/11/25.
+//
+
+import SwiftUI
+
+@main
+struct RickMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
