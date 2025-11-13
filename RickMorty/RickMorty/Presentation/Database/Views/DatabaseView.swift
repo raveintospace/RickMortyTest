9efa089bfdfe1359a93686f2d245aae05944b425
Oriol Angelet <uriaeri@gmail.com>
2025-11-13 20:52:43 +0100

    Added in DatabaseView: databaseWallpaper & alert when there aren't more characters to fetch

--- RickMorty/RickMorty/Presentation/Database/Views/DatabaseView.swift
+++ RickMorty/RickMorty/Presentation/Database/Views/DatabaseView.swift
@@ -11,29 +11,25 @@ struct DatabaseView: View {
     
     @Environment(\.databaseViewModel) private var databaseViewModel
     
+    @State private var showAlertOnEndOfList: Bool = false
+    
     var body: some View {
         NavigationStack {
             ZStack {
-                Color.rmYellow.ignoresSafeArea().opacity(0.1)
+                databaseWallpaper
                 
                 ScrollView {
-                    LazyVGrid(
-                        columns: Array(repeating: GridItem(.flexible(), spacing: 10), count: 2),
-                        alignment: .center,
-                        spacing: 10,
-                        pinnedViews: [],
-                        content: {
-                            ForEach(databaseViewModel.characters) { character in
-                                DatabaseCard(character: character)
-                            }
-                        }
-                    )
-                    .padding(.horizontal)
+                    displayedCardsGrid
                 }
                 
             }
+            .toolbar(.hidden, for: .navigationBar)
+            .persistentSystemOverlays(.hidden)
             .task {
                 await databaseViewModel.loadCharacters()
+            }
+            .alert(isPresented: $showAlertOnEndOfList) {
+                allCharactersLoadedAlert()
             }
         }
     }
@@ -45,3 +41,57 @@ struct DatabaseView: View {
         .environment(\.databaseViewModel, DeveloperPreview.instance.databaseViewModel)
 }
 #endif
+
+extension DatabaseView {
+    
+    // MARK: - View components
+    private var databaseWallpaper: some View {
+        Image("databaseWallpaper")
+            .resizable()
+            .ignoresSafeArea()
+            .opacity(0.15)
+    }
+    
+    private var displayedCardsGrid: some View {
+        LazyVGrid(
+            columns: Array(repeating: GridItem(.flexible(), spacing: 10), count: 2),
+            alignment: .center,
+            spacing: 10,
+            pinnedViews: [],
+            content: {
+                ForEach(databaseViewModel.characters) { character in
+                    DatabaseCard(character: character,
+                                 onCardPressed: {
+                        // go to detailview
+                    }
+                    )
+                    .onAppear {
+                        if character == databaseViewModel.characters.last {
+                            handleEndOfList()
+                        }
+                    }
+                }
+            }
+        )
+        .padding()
+    }
+    
+    // MARK: - Private methods
+    private func allCharactersLoadedAlert() -> Alert {
+        return Alert(
+            title: Text("All characters are loaded"),
+            message: Text("You have \(databaseViewModel.characters.count) characters available."),
+            dismissButton: .default(Text("OK"))
+        )
+    }
+    
+    private func handleEndOfList() {
+        if databaseViewModel.canLoadMore {
+            Task {
+                await databaseViewModel.loadCharacters()
+            }
+        } else {
+            showAlertOnEndOfList = true
+        }
+    }
+}
