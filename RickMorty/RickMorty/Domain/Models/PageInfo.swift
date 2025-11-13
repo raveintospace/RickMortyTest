c1de689b823702a03817b28c6e4975de1d254f0d
Oriol Angelet <uriaeri@gmail.com>
2025-11-13 09:34:02 +0100

    Created PageInfo model

---
+++ RickMorty/RickMorty/Domain/Models/PageInfo.swift
@@ -0,0 +1,29 @@
+//
+//  PageInfo.swift
+//  RickMorty
+//
+//  Created by Uri on 13/11/25.
+//
+
+import Foundation
+
+/// The essential pagination metadata
+struct PageInfo: Decodable {
+    let characterCount: Int
+    let nextPage: URL?
+    
+    enum CodingKeys: String, CodingKey {
+        case characterCount = "count"
+        case nextPage = "next"
+    }
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.characterCount = try container.decode(Int.self, forKey: .characterCount)
+        
+        // Api returns nil when we receive the last page available
+        let nextString = try container.decodeIfPresent(String.self, forKey: .nextPage)
+        self.nextPage = nextString.flatMap { URL(string: $0) }
+    }
+}
