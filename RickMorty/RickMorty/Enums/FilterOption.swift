50d961fbb3b66b3ee898be97f2cff781657ef6b0
Oriol Angelet <uriaeri@gmail.com>
2025-11-13 21:19:31 +0100

    Created enums for Filter & Sort

---
+++ RickMorty/RickMorty/Enums/FilterOption.swift
@@ -0,0 +1,23 @@
+//
+//  FilterOption.swift
+//  RickMorty
+//
+//  Created by Uri on 13/11/25.
+//
+
+import Foundation
+
+enum FilterOption: String, CaseIterable, Identifiable {
+    case gender, status
+    
+    var id: String { self.rawValue }
+    
+    var displayName: String {
+        switch self {
+        case .gender:
+            "Gender"
+        case .status:
+            "Status"
+        }
+    }
+}
---
+++ RickMorty/RickMorty/Enums/SortOption.swift
@@ -0,0 +1,51 @@
+//
+//  SortOption.swift
+//  RickMorty
+//
+//  Created by Uri on 13/11/25.
+//
+
+import SwiftUI
+
+enum SortOption: String, CaseIterable {
+    case id, idReversed, name, nameReversed
+    
+    private var baseName: String {
+        switch self {
+        case .id, .idReversed:
+            return "ID"
+        case .name, .nameReversed:
+            return "Name"
+        }
+    }
+    
+    private var isReversed: Bool {
+        switch self {
+        case .idReversed, .nameReversed:
+            return true
+        default:
+            return false
+        }
+    }
+    
+    func displayName() -> some View {
+        HStack(spacing: 4) {
+            Text(baseName)
+            Image(systemName: self.isReversed ? "chevron.up" : "chevron.down")
+        }
+        .accessibilityElement(children: .combine)
+        .accessibilityLabel(accessibilityDescription)
+        .accessibilityAddTraits(.isStaticText)
+    }
+    
+    var accessibilityDescription: String {
+        let orderDescription = isReversed ? "descending" : "ascending"
+
+        switch self {
+        case .id, .idReversed:
+            return "Sort by character ID, \(orderDescription)"
+        case .name, .nameReversed:
+            return "Sort by Name, \(orderDescription)"
+        }
+    }
+}
