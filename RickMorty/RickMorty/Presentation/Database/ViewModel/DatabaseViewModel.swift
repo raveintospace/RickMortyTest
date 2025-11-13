9a9c5f4e470eb4a59ddfb28a98b8737eccfc5393
Oriol Angelet <uriaeri@gmail.com>
2025-11-13 12:50:11 +0100

    Created DatabaseViewModel

---
+++ RickMorty/RickMorty/Presentation/Database/ViewModel/DatabaseViewModel.swift
@@ -0,0 +1,66 @@
+//
+//  DatabaseViewModel.swift
+//  RickMorty
+//
+//  Created by Uri on 13/11/25.
+//
+
+import Foundation
+
+@Observable
+final class DatabaseViewModel {
+    
+    // MARK: - Properties
+    private(set) var characters: [CardCharacter] = []
+    private(set) var totalCharacterCount: Int = 0
+    private(set) var isLoading: Bool = false
+    private(set) var canLoadMore: Bool = true
+    
+    private(set) var currentPage: Int = 1
+    
+    private(set) var errorMessage: String?
+    
+    var downloadedCharactersCount: Int {
+        characters.count
+    }
+    
+    // MARK: - Dependency injection
+    private let fetchCharactersUseCase: FetchCardCharactersUseCaseProtocol
+    
+    init(fetchCharactersUseCase: FetchCardCharactersUseCaseProtocol) {
+        self.fetchCharactersUseCase = fetchCharactersUseCase
+    }
+    
+    // MARK: - Public methods
+    func loadCharacters() async {
+        guard !isLoading && canLoadMore else { return }
+        
+        await MainActor.run {
+            self.isLoading = true
+            self.errorMessage = nil // Clean previous errors
+        }
+        
+        do {
+            let response = try await fetchCharactersUseCase.execute(page: currentPage)
+            
+            await MainActor.run {
+                self.characters.append(contentsOf: response.results)
+                
+                self.totalCharacterCount = response.info.characterCount
+                
+                // Update properties for the next fetch
+                self.canLoadMore = response.info.nextPage != nil
+                self.currentPage += 1
+            }
+        } catch {
+            await MainActor.run {
+                self.errorMessage = "Error loading characters on page \(self.currentPage): \(error.localizedDescription)"
+                self.canLoadMore = false
+            }
+        }
+        
+        await MainActor.run {
+            self.isLoading = false
+        }
+    }
+}
