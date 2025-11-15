4bb44d209bb7a28f4e862f4ddb3e32006ffdee84
Oriol Angelet <uriaeri@gmail.com>
2025-11-15 16:58:29 +0100

    First iteration of DetailViewModel

---
+++ RickMorty/RickMorty/Presentation/Detail/ViewModel/DetailViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  DetailViewModel.swift
+//  RickMorty
+//
+//  Created by Uri on 15/11/25.
+//
+
+import Foundation
+
+@Observable
+final class DetailViewModel {
+    
+    private(set) var character: DetailCharacter?
+    private(set) var isLoading: Bool = false
+    private(set) var errorMessage: String?
+    
+    private let characterID: Int
+    
+    // MARK: - Dependency injection
+    private let fetchDetailCharacterUseCase: FetchDetailCharacterUseCaseProtocol
+    
+    init(characterID: Int,
+        fetchDetailCharacterUseCase: FetchDetailCharacterUseCaseProtocol
+    ) {
+        self.characterID = characterID
+        self.fetchDetailCharacterUseCase = fetchDetailCharacterUseCase
+    }
+    
+    @MainActor
+    func loadCharacter() async {
+        guard !isLoading else { return }
+        
+        isLoading = true
+        errorMessage = nil
+        
+        do {
+            let fetchedCharacter = try await fetchDetailCharacterUseCase.execute(id: characterID)
+            
+            self.character = fetchedCharacter
+        } catch {
+            self.errorMessage = "Error loading the character: \(error.localizedDescription)"
+        }
+        
+        self.isLoading = false
+    }
+}
