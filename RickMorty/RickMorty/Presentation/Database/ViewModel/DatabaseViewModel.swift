318cfa0261a39f317b3428b5270735ec6d8e2a26
Oriol Angelet <uriaeri@gmail.com>
2025-11-15 09:43:59 +0100

    Added logic in DatabaseViewModel to search & filter

--- RickMorty/RickMorty/Presentation/Database/ViewModel/DatabaseViewModel.swift
+++ RickMorty/RickMorty/Presentation/Database/ViewModel/DatabaseViewModel.swift
@@ -6,39 +6,60 @@
 //
 
 import Foundation
+import Combine
 
 @Observable
 final class DatabaseViewModel {
     
-    // MARK: - Properties
+    // MARK: - Basic properties
     private(set) var fetchedCharacters: [CardCharacter] = []
     private(set) var totalCharacterCount: Int = 0
-    private(set) var isLoading: Bool = false
-    private(set) var canFetchMore: Bool = true
-    private(set) var currentPage: Int = 1
-    private(set) var errorMessage: String?
     
     var fetchedCharactersCount: Int {
         fetchedCharacters.count
     }
     
+    var displayedCharacters: [CardCharacter] {
+        return sortCharacters(characters: filterCharacters(searchText: searchText,
+                                                           selectedFilter: selectedFilter,
+                                                           characters: fetchedCharacters))
+    }
+    
+    // MARK: - Fetch properties
+    private(set) var isLoading: Bool = false
+    private(set) var canFetchMore: Bool = true
+    private(set) var currentPage: Int = 1
+    private(set) var errorMessage: String?
+    
     // MARK: - Search characters
-    var searchText: String = ""
+    @Published var searchText: String = ""
     
     var isSearching: Bool {
         !searchText.isEmpty
     }
     
-    // MARK: - Computed properties to react when filtering or searching
+    // MARK: - Computed properties for UI when filtering or searching
     var isFilteringOrSearching: Bool {
         return selectedFilter != nil || !searchText.isEmpty
+    }
+    
+    var noResultsForFilter: Bool {
+        return displayedCharacters.isEmpty && selectedFilter != nil
+    }
+    
+    var noResultsForSearchText: Bool {
+        return displayedCharacters.isEmpty && !searchText.isEmpty
+    }
+    
+    var showNoResultsView: Bool {
+        return noResultsForFilter || noResultsForSearchText
     }
     
     // MARK: - Filters for characters
     private(set) var genderFilters: [Filter] = []
     private(set) var statusFilters: [Filter] = []
-    var selectedFilter: Filter? = nil    // A gender or a status
-    var selectedFilterOption: FilterOption = .gender {
+    @Published var selectedFilter: Filter? = nil    // A gender or a status
+    @Published var selectedFilterOption: FilterOption = .gender {
         didSet {
             if oldValue != selectedFilterOption {
                 selectedFilter = nil        // Reset it when changing selectedFilterOption
@@ -51,7 +72,10 @@ final class DatabaseViewModel {
     private(set) var activeSubfilters: [Filter] = []  // Genders (4) or Status (3)
     
     // MARK: - Sort order for characters
-    var sortOption: SortOption = .id
+    @Published var sortOption: SortOption = .id
+    
+    // Combine
+    private var cancellables = Set<AnyCancellable>()
     
     // MARK: - Dependency injection
     private let fetchCardCharactersUseCase: FetchCardCharactersUseCaseProtocol
@@ -62,6 +86,8 @@ final class DatabaseViewModel {
     ) {
         self.fetchCardCharactersUseCase = fetchCardCharactersUseCase
         self.getFiltersUseCase = getFiltersUseCase
+        
+        addSubscribers()
         
         Task {
             await updateActiveSubfilters()
@@ -124,6 +150,71 @@ final class DatabaseViewModel {
         case .status:
             await loadStatusFilters()
             activeSubfilters = statusFilters
+        }
+    }
+    
+    // MARK: - Combine subscriptions
+    private func addSubscribers() {
+        $searchText
+            .combineLatest($selectedFilter, $selectedFilterOption, $sortOption)
+            .debounce(for: 0.3, scheduler: DispatchQueue.main)
+            .sink { [weak self] (searchText, selectedFilter, selectedFilterOption, sortOption) in
+                guard let self = self else { return }
+                // property displayedCharacters calls the logic
+            }
+            .store(in: &cancellables)
+        
+        $selectedFilterOption
+            .sink { [weak self] filterOption in
+                guard let self = self else { return }
+                Task {
+                    await self.updateActiveSubfilters()
+                }
+            }
+            .store(in: &cancellables)
+    }
+    
+    // MARK: - Filter logic, combining searchText & Filters
+    private func filterCharacters(searchText: String,
+                                  selectedFilter: Filter?,
+                                  characters: [CardCharacter]) -> [CardCharacter] {
+        var filtered = characters
+        
+        // SearchText filtering
+        if !searchText.isEmpty {
+            let search = searchText.lowercased()
+            
+            filtered = filtered.filter { character in
+                let nameContainsSearch = character.name.lowercased().contains(search)
+                let genderContainsSearch = character.gender.rawValue.lowercased().contains(search)
+                let statusContainsSearch = character.status.rawValue.lowercased().contains(search)
+                let speciesContainsSearch = character.species.lowercased().contains(search)
+                let typeContainsSearch = character.type.lowercased().contains(search)
+                
+                return nameContainsSearch || genderContainsSearch || statusContainsSearch || speciesContainsSearch ||
+                typeContainsSearch
+            }
+        }
+        
+        // Apply Filters (Gender or Status)
+        if let filter = selectedFilter {
+            filtered = applyFilter(filter: filter, characters: filtered)
+        }
+        
+        return filtered
+    }
+    
+    // MARK: - Filter logic with Filters only
+    private func applyFilter(filter: Filter, characters: [CardCharacter]) -> [CardCharacter] {
+        let filterTitle = filter.title.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        return characters.filter { character in
+            switch selectedFilterOption {
+            case .gender:
+                return character.gender.rawValue.lowercased().trimmingCharacters(in: .whitespacesAndNewlines) == filterTitle
+            case .status:
+                return character.status.rawValue.lowercased().trimmingCharacters(in: .whitespacesAndNewlines) == filterTitle
+            }
         }
     }
     
