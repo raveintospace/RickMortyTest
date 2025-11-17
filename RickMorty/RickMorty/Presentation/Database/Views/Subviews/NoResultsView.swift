d6aaf56c21dc78c2676d769e8bf875d9a2d1b5be
Oriol Angelet <uriaeri@gmail.com>
2025-11-17 16:56:45 +0100

    Added lime shadow to NoResultsView

--- RickMorty/RickMorty/Presentation/Database/Views/Subviews/NoResultsView.swift
+++ RickMorty/RickMorty/Presentation/Database/Views/Subviews/NoResultsView.swift
@@ -36,6 +36,7 @@ struct NoResultsView: View {
             Text(callToActionText)
                 .font(.callout)
         }
+        .shadow(color: .rmLime, radius: 1)
         .modifier(GlassSheetModifier())
         .accessibilityElement(children: .combine)
         .accessibilityLabel("\(mainText). \(callToActionText).")
