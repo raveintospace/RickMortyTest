6480079e3a4d181d3489a6f1be33626ff1771135
Oriol Angelet <uriaeri@gmail.com>
2025-11-17 13:01:54 +0100

    Created EpisodeCard // Updated computed string of Episode model

---
+++ RickMorty/RickMorty/Presentation/EpisodeList/Views/Subviews/EpisodeCard.swift
@@ -0,0 +1,61 @@
+//
+//  EpisodeCard.swift
+//  RickMorty
+//
+//  Created by Uri on 17/11/25.
+//
+
+import SwiftUI
+
+struct EpisodeCard: View {
+    
+    @Environment(\.isPad) var isPad: Bool
+    
+    let episode: Episode
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            episodeTitle
+            
+            InsightRow(label: "Aired on", value: episode.airDate)
+            InsightRow(label: "Episode code", value: episode.episodeCode)
+                .padding(.bottom, 8)
+            
+            episodeCharacterCountLabel
+        }
+        .font(isPad ? .title : .headline)
+        .shadow(color: .rmLime, radius: 1)
+        .modifier(GlassSheetModifier())
+        .shadow(color: .rmLime, radius: 3)
+        .accessibilityElement(children: .combine)
+    }
+}
+
+#if DEBUG
+#Preview {
+    EpisodeCard(episode: Episode.Stub.stub1)
+}
+#endif
+
+extension EpisodeCard {
+    
+    private var episodeTitle: some View {
+        Text(episode.name)
+            .font(isPad ? .largeTitle : .title3)
+            .lineLimit(3)
+            .underline()
+            .multilineTextAlignment(.leading)
+            .minimumScaleFactor(0.7)
+            .bold()
+            .padding(.bottom, 8)
+            .accessibilityAddTraits(.isHeader)
+            .accessibilityLabel("Episode title: \(episode.name)")
+    }
+    
+    private var episodeCharacterCountLabel: some View {
+        Text(episode.characterCountText)
+            .lineLimit(2)
+            .minimumScaleFactor(0.7)
+            .accessibilityLabel(episode.characterCountText)
+    }
+}
