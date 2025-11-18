e6a41f85f27f8e3bb83e7ba1279edbdd30a32f4b
Oriol Angelet <uriaeri@gmail.com>
2025-11-18 17:55:01 +0100

    Added Equatable conformance to enum RemoteDataSourceError

--- RickMorty/RickMorty/Enums/RemoteDataSourceError.swift
+++ RickMorty/RickMorty/Enums/RemoteDataSourceError.swift
@@ -6,9 +6,24 @@
 //
 
 /// The specific errors that could happen when fetching remote data
-enum RemoteDataSourceError: Error {
+enum RemoteDataSourceError: Error, Equatable {
     case badServerResponse
     case decodingError(Error)
     case httpError(statusCode: Int)
     case invalidURL
+    
+    static func == (lhs: RemoteDataSourceError, rhs: RemoteDataSourceError) -> Bool {
+        switch (lhs, rhs) {
+        case (.badServerResponse, .badServerResponse):
+            return true
+        case (.invalidURL, .invalidURL):
+            return true
+        case (.httpError(let lhsCode), .httpError(let rhsCode)):
+            return lhsCode == rhsCode
+        case (.decodingError, .decodingError):
+            return true
+        default:
+            return false
+        }
+    }
 }
